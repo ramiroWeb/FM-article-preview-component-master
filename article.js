351a5fda0665213619056ba205bd371567e67c50
rami <ramiro_coronel333@hotmail.com>
2024-08-08 17:34:39 -0300

    finish css for mobile

--- article.js
+++ article.js
@@ -1,13 +1,17 @@
 const articleButton = document.getElementById("article-button");
 
 const changeDisplayBottom = () => {
-  const hiddenGroup = document.querySelector(".group-hidden");
-  const defaultGroup = document.querySelector(".group-default");
+  const hiddenGroup = document.querySelector(".hidden-group");
+  const defaultGroup = document.querySelector(".default-group");
   const articleBottom = document.querySelector(".article__bottom");
 
   hiddenGroup.classList.toggle("hidden");
   defaultGroup.classList.toggle("hidden");
-  articleBottom.classList.toggle("bottom-active");
+  articleBottom.classList.toggle("bottom-socials");
+  console.log("ejecutando");
+  buttonDisplay();
 };
-
+const buttonDisplay = () => {
+  articleButton.classList.toggle("button-active");
+};
 articleButton.addEventListener("click", (e) => changeDisplayBottom());
